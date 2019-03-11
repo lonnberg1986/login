034f9a076df2fd2f76965d4b3de0eb9a06626a74
Jani Lönnberg <jani.lonnberg@protonmail.com>
2019-03-11 11:13:20 +0200

    Initial commit for login v1.0

---
+++ README.md
@@ -0,0 +1,8 @@
+# Sign In page v1.0
+This is the v1.0 for a demo Sign In page.
+Key features:
+* Responsive Design for all devices
+* uses SVG logo
+* Custom build form layout
+* Used flexbox css design
+[http://programmingsandbox.fi/login.php](http://programmingsandbox.fi/login.php)
---
+++ login.php
@@ -0,0 +1,32 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<link rel="stylesheet" href="./resources/login_styles.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+	<title>Welcome!</title>
+</head>
+<body>
+	<div class="wrapper">
+		<div class="flex-container">
+			<div class="container">
+				<div class="form-header">
+					<h1><i class="fas fa-user-lock"></i>Sign In</h1>
+				</div>
+				<div class="form-body">
+					<form action="welcome.html" method="POST">
+						<label for="username">Username:</label>
+						<input type="text" name="username" id="username" placeholder="Username">
+						<label for="password">Password:</label>
+						<input type="password" name="password" id="password" placeholder="Password">
+						<input type="submit" name="login" value="Sign in">
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
