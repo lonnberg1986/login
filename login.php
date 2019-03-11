8723784962c884e47ac4c714d6baa510a054a140
Jani Lönnberg <jani.lonnberg@protonmail.com>
2019-03-11 11:46:57 +0200

    Require username and password for login

--- login.php
+++ login.php
@@ -19,9 +19,9 @@
 				<div class="form-body">
 					<form action="welcome.html" method="POST">
 						<label for="username">Username:</label>
-						<input type="text" name="username" id="username" placeholder="Username">
+						<input type="text" name="username" id="username" placeholder="Username" required="required">
 						<label for="password">Password:</label>
-						<input type="password" name="password" id="password" placeholder="Password">
+						<input type="password" name="password" id="password" placeholder="Password" required="required">
 						<input type="submit" name="login" value="Sign in">
 					</form>
 				</div>
